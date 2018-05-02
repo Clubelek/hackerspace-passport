e474876c5d7c059820b5596b84745a6388d66efb
Nathan/Eilisha Shiraini <neshiraini@heptacle.fr>
2018-05-02 17:10:10 +0200

    Automated page organization

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 FFX=firefox -headless
 PY=python3
 IMM=convert
+PPU=pdfunite
 
 SVGDIR=svg
 PNGDIR=png
@@ -10,6 +11,9 @@ HTMDIR=html
 SCRDIR=scripts
 TMPDIR=templates
 PDFDIR=pdf
+
+include cover.mk
+include pages.mk
 
 $(PNGDIR)/%.png : $(HTMDIR)/%.html
 	@test -d $$(dirname $@) || mkdir -p $$(dirname $@)
@@ -24,6 +28,9 @@ $(PDFDIR)/%.pdf : $(PNGDIR)/%.png
 	$(IMM) -density 254 -units pixelsperinch $< $@
 
 $(PNGDIR)/full_cover.png : $(SVGDIR)/full_cover.svg $(SVGDIR)/front_cover.svg $(SVGDIR)/back_cover.svg $(SVGDIR)/binding_cover.svg $(SVGDIR)/cropmarks_cover.svg
+
+pages.mk cover.mk : $(SCRDIR)/organizer.py
+	$(PY) $< svg/pages $@
 
 .SECONDEXPANSION:
 $(HTMDIR)/pages_%.html : $(SCRDIR)/pager.py $(TMPDIR)/pages_template.html $(SVGDIR)/pages/p_$$(word 1,$$(subst _, ,$$*)).svg $(SVGDIR)/pages/p_$$(word 2,$$(subst _, ,$$*)).svg $(SVGDIR)/page_background.svg $(SVGDIR)/cropmarks_pages.svg
---
+++ scripts/organizer.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import re
+import math
+import os.path
+import itertools
+
+DEFAULT_FILES = {
+	"cover" : "$(SVGDIR)/pages/c_n.svg",
+	"pages" : "$(SVGDIR)/pages/p_n.svg",
+}
+
+BUILD_RULE = "\t$(PPU) $^ $@\n"
+
+def listfiles(path, type):
+	lst = (f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and re.match(r'^[cp]_[0-9]+\.svg$', f))
+	result = []
+	for f in lst:
+		r = {
+			"path" : os.path.join(path, f),
+			"number" : int(os.path.splitext(os.path.basename(f))[0].split('_')[-1]),
+			"type" : {
+				"c" : "cover",
+				"p" : "pages"
+			}[os.path.splitext(os.path.basename(f))[0].split('_')[0]]
+		}
+		result.append(r)
+	return result if type is 'both' else [f for f in result if f['type'] == type]
+
+def makedeps(pagelist):
+	lst = sorted(pagelist, key= lambda d:d['number'])
+	pagecount = len(lst)
+	result = []
+	for i in range(math.ceil(pagecount/2)):
+		left = None if i > pagecount else i
+		right = None if pagecount - (i+1) > pagecount else pagecount - (i+1)
+		if left is not None and right is not None and lst[left]['type'] != lst[right]['type']:
+			raise ValueError("Pages %s and %s should be matched but have different type" % (lst[left]['path'], lst[right]['path']))
+		result.append('$(PDFDIR)/' + lst[left]['type'] + '_' + ('n' if left is None else str(left)) + '_' + ('n' if right is None else str(right)) + '.pdf')
+	return result
+
+if __name__ == "__main__":
+	pagedir = sys.argv[1]
+	mode = sys.argv[2].split('.')[0]
+	lst = listfiles(pagedir, mode)
+	types = {d['type'] for d in lst}
+	for t in types:
+		with open('%s.mk' % t, 'w', encoding='utf8') as f:
+			f.write('$(PDFDIR)/%s.pdf' % t)
+			f.write(' : ')
+			f.write(' '.join(makedeps(d for d in lst if d['type'] == t)))
+			f.write('\n')
+			f.write(BUILD_RULE)
